e0e086359fced79c1af94980c9f0765ab1e1e81b
pld9bc_112816 <pld9bc@mail.missouri.edu>
2015-02-28 10:52:33 -0600

    .....

--- index.php
+++ index.php
@@ -8,7 +8,40 @@
 	<!-- Bootstrap theme -->
 	<link href="include/bootstrap/css/bootstrap-theme.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="include/styles.css">
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="./include/main.js"></script>	
 	<title>trvBud</title>
+	<script>
+/*$('#city-in').bind("keydown", function() {
+	console.dir("boop");
+	populate();
+});*/
+
+$( "#city-in" ).keydown(function() {
+	alert( "Handler for .keydown() called." );
+});
+
+// function to be called on every key press in the text-box
+function populate(){
+	var input = $('#city-in').val();
+	console.dir(input);
+	options = {
+		"url": "https://babbage.cs.missouri.edu/~pld9bc/hackillinois/trvBud/include/util/autoTextbox.php",
+		"content": "data",
+		"format": "json"
+	};
+	$.get("https://babbage.cs.missouri.edu/~pld9bc/hackillinois/trvBud/include/util/autoTextbox.php?search="+input, options, function(data) {
+		processData(data);
+	});// end get
+}// end populate
+
+function processData( data ){
+	console.dir(data);
+}// end processData
+
+	</script>
+	
 </head>
 <body id="body">
 	<!--
@@ -35,7 +68,7 @@
 		<center>
 		<br>
 		<div class="input-group">
-		  <input type="text" class="form-control" placeholder="Name or Zipcode.." aria-describedby="basic-addon1">
+		  <input type="text" class="form-control" placeholder="Name or Zipcode.." aria-describedby="basic-addon1" id="city-in" name="city-in">
 		</div>
 		<br>
 		<!--Adding a new part-->
@@ -48,4 +81,5 @@
 	<!-- run scripts at end of page for faster loading -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="include/bootstrap/dist/js/bootstrap.min.js"></script>
+   	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
  </body>
